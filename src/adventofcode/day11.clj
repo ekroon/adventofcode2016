f32a0a6cbd0ac3b0636ac0d3455e08b641233f0b
Erwin Kroon <erwin+git@k3n.nl>
2016-12-13 12:16:56 +0100

    Day 11 not finishing

--- src/adventofcode/day11.clj
+++ src/adventofcode/day11.clj
@@ -19,6 +19,35 @@
    4 {:items #{}
       :floor 4}})
 
+(def assignment
+  {1 {:current true
+      :floor 1
+      :items #{{:type :generator
+                :element :polonium}
+               {:type :generator
+                :element :thulium}
+               {:type :microchip
+                :element :thulium}
+               {:type :generator
+                :element :promethium}
+               {:type :generator
+                :element :ruthenium}
+               {:type :microchip
+                :element :ruthenium}
+               {:type :generator
+                :element :cobalt}
+               {:type :microchip
+                :element :cobalt}}}
+   2 {:floor 2
+      :items #{{:type :microchip
+                :element :polonium}
+               {:type :microchip
+                :element :promethium}}}
+   3 {:floor 3
+      :items #{}}
+   4 {:floor 4
+      :items #{}}})
+
 (defn valid-floor? [floor]
   (let [generator? (not (every? #(= :microchip (:type %)) (:items floor)))]
     (if generator?
@@ -61,8 +90,49 @@
         only-valid (filter valid-state? new-states)]
     only-valid))
 
+(defn is-final? [state]
+  (let [result
+        (every? (comp empty? :items (partial get state)) [1 2 3])]
+    ;;(println result)
+    result))
+
+(defn solver [state]
+  (loop [queue [{:state state :steps []}]
+         seen  {}]
+    (let [state (-> queue first :state)
+          steps (-> queue first :steps)]
+      (when (= 0 (mod (count seen) 100))
+        (println (count seen) " " (count queue)))
+      (cond (not state)
+            (do
+              #_(dorun (map (fn [[k v]] (println k)) seen))
+              (let [result (into {} (filter (fn [[k v]] (is-final? k)) seen))]
+                #_(println (first (first result)))
+                result))
+
+            (get seen state)
+            (do
+              (recur (rest queue) seen))
+
+            (is-final? state)
+            (do
+              (recur (rest queue) (assoc seen state (first queue))))
+
+            state
+            (do
+              (let [add (map (fn [s] {:state s :steps (concat steps [state])})
+                             (remove #(get seen %) (generate-steps state)))]
+                (println "add to queue: " (count add))
+                (println "added: " add)
+                (recur (concat (rest queue) add)
+                       (assoc seen state (first queue)))))
+
+            :else
+            (assert false)))))
+
 (defn solve [input]
-  (generate-steps example))
+  (-> (solver example)
+      vals first :steps count))
 
 (defn -main [& args]
   (println (solve input)))
